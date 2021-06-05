46f4b6bc1385cd367ad373016b22a1b79a917db7
Vasya N <n1m5spm@yandex.com>
2021-06-05 21:26:48 +0200

    Add default wesnoth versions in the skeleton

--- skeleton/.build/config.sh
+++ skeleton/.build/config.sh
@@ -1,2 +1,4 @@
 # You can override `wesnoth_upload` behavior with this file.
 # If you're interested in that, check out "config_example.sh" and copy needed overrides here.
+
+upload_to_wesnoth_versions=(1.14) # (1.14 1.15)
